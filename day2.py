7fdd3bea7201413b21a97d8a137c14211c380402
Jon Mockbee <mockbee1000@gmail.com>
2021-07-16 10:34:06 -0400

    this is acutally day 3

--- day2.py
+++ day2.py
@@ -3,3 +3,9 @@ mary =5
 adam =6
 total_apples = (adam + mary + jon) 
 print(total_apples, "total number of apples" )
+
+x =  -1
+x = float(x)  
+# y= 3x3 - 2x2 + 3x - 1
+y=3*(x ** 3) - 2*(x ** 2) + 3*x -1
+print("y =", y)
